cd10c80623d83dae4f0b3fa643507bf491d4c818
Muhammadqodir0903 <madinaziemova@gmail.com>
2023-11-23 22:46:34 +0500

    Sayohat uchun

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var a = prompt('Sayohat uchun ajratgan pulingiz');
+var elTitle = document.querySelector('.title');
+if (a == 9570000 ) {
+    elTitle.textContent = 'Sizga oqyol tilaymiz  Safaringiz bexatar bolsin';
+}else{
+        elTitle.textContent = 'Hurmatli mijoz sayohat narxi 9570000 sum';
+    }if (a = '') {
+        elTitle.textContent = 'Hurmatli mijoz tolov usulini raqamlar bilan ifodalang!';
+    };
+
